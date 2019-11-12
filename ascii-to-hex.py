79bf25f720bb10163a8418f0161e2547cfa69c70
Kevin Van Liebergen Avila <kevinliebergen@hotmail.com>
2019-11-12 15:09:52 +0100

    asciied added

--- ascii-to-hex.py
+++ ascii-to-hex.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 #text = "<script>alert('1')</script>"
 text = raw_input("Introduce codigo a ofuscar: ")
 
-print(text)
-
-
 splitted = list(text)
 hexed = ""
+asciied = ""
 
 for i in splitted:
     hexed += '%' + hex(ord(i))[2:]
+    #hexed += '\ x' + hex(ord(i))[2:]
+    asciied += '&#' + str(ord(i))
 
-print(hexed)
-
+print "hexed: ", hexed
+print "asciied: ", asciied
